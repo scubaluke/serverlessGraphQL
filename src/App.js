ae30fca5cbe1eb3328b727626babab9adf3f44ae
Luke <lukegartland@gmail.com>
2021-09-23 17:12:23 -0700

    calling api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
-//https://www.youtube.com/watch?v=HZUlQ7Z2xHQ
+import { listTalks as ListTalks } from './graphql/queries'
+import { API, graphqlOperation } from 'aws-amplify'
+
+//video: https://www.youtube.com/watch?v=HZUlQ7Z2xHQ
 function App() {
+  const [talks, setTalks] = useState([])
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  async function getData() {
+    try {
+      const talkData = await API.graphql(graphqlOperation(ListTalks))
+      console.log('talkData:', talkData)
+      setTalks(talkData.data.listTalks.items)
+    } catch (error) {
+      console.log('error fetching talks...', error)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        talks.map((talk, index) => (
+          <div>
+            <h3>{talk.speakerName}</h3>
+            <h5>{talk.name}</h5>
+            <p>{talk.description}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
